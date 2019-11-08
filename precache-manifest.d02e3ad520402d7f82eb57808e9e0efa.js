8cd6e1234764449b91d339206ccb4af4a95ad58e
OlgaSavytska <o.savytska77@gmail.com>
2019-11-08 16:04:45 +0200

    Updates

---
+++ precache-manifest.d02e3ad520402d7f82eb57808e9e0efa.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf99b7d816548413effb69ee4e6a3df",
+    "url": "/go_to_goal/index.html"
+  },
+  {
+    "revision": "e6cff8ab29c219ec7258",
+    "url": "/go_to_goal/static/css/2.4b3442db.chunk.css"
+  },
+  {
+    "revision": "0efcae81c1eb88f994e8",
+    "url": "/go_to_goal/static/css/main.150f9daa.chunk.css"
+  },
+  {
+    "revision": "e6cff8ab29c219ec7258",
+    "url": "/go_to_goal/static/js/2.fd7b23ba.chunk.js"
+  },
+  {
+    "revision": "0efcae81c1eb88f994e8",
+    "url": "/go_to_goal/static/js/main.53be1395.chunk.js"
+  },
+  {
+    "revision": "c537adee366154bb694a",
+    "url": "/go_to_goal/static/js/runtime~main.e05fd584.js"
+  },
+  {
+    "revision": "1c2a13f92c9da459f2ff4d99109e3184",
+    "url": "/go_to_goal/static/media/FlagsIcon.1c2a13f9.png"
+  },
+  {
+    "revision": "718c9449dbc1333828a67495aaa08c5d",
+    "url": "/go_to_goal/static/media/add-plus-button.718c9449.svg"
+  },
+  {
+    "revision": "dbe6fb2412fa5e2bf21ba83ed4c59863",
+    "url": "/go_to_goal/static/media/arrow-down.dbe6fb24.svg"
+  },
+  {
+    "revision": "321ba643528ecd2212a14d34840c2e11",
+    "url": "/go_to_goal/static/media/big-arrow.321ba643.png"
+  },
+  {
+    "revision": "7d4fa344c12c96c4fe47353f5f5b6fd1",
+    "url": "/go_to_goal/static/media/close.7d4fa344.svg"
+  },
+  {
+    "revision": "50bda4bb1d2c16abcb3ef0189b2a79e6",
+    "url": "/go_to_goal/static/media/closeEye.50bda4bb.svg"
+  },
+  {
+    "revision": "1b058b200464739aa028187859cabb32",
+    "url": "/go_to_goal/static/media/decor.1b058b20.png"
+  },
+  {
+    "revision": "862fe63fc5fbbfb5b7906d34b81e4ec7",
+    "url": "/go_to_goal/static/media/gift.862fe63f.svg"
+  },
+  {
+    "revision": "3e617171ca7092f3852cee9aa6081937",
+    "url": "/go_to_goal/static/media/indie-flower-v11-latin-regular.3e617171.ttf"
+  },
+  {
+    "revision": "c0cf017f1a059881d1438b269135198d",
+    "url": "/go_to_goal/static/media/indie-flower-v11-latin-regular.c0cf017f.woff2"
+  },
+  {
+    "revision": "c2cc9efffb3660e9e031e4971f29fb07",
+    "url": "/go_to_goal/static/media/indie-flower-v11-latin-regular.c2cc9eff.woff"
+  },
+  {
+    "revision": "e125898f5bc47cbb04ef2f7f4dac2a10",
+    "url": "/go_to_goal/static/media/indie-flower-v11-latin-regular.e125898f.eot"
+  },
+  {
+    "revision": "ecfda83eaa1e552110f51fca20cb3d2c",
+    "url": "/go_to_goal/static/media/indie-flower-v11-latin-regular.ecfda83e.svg"
+  },
+  {
+    "revision": "4b76132f984a9df4f4e32cae0f8dfc50",
+    "url": "/go_to_goal/static/media/login-page-cover@1X.4b76132f.png"
+  },
+  {
+    "revision": "eac11c355bba74acc5f7c7fd6b49e627",
+    "url": "/go_to_goal/static/media/logout.eac11c35.svg"
+  },
+  {
+    "revision": "0daf69c3f160c2bae862677677240739",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-500.0daf69c3.ttf"
+  },
+  {
+    "revision": "10c07b65ea6f6c0eccf3235671353687",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-500.10c07b65.svg"
+  },
+  {
+    "revision": "52a694238a064633d2350463f8f29191",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-500.52a69423.eot"
+  },
+  {
+    "revision": "a3f8d239529a62d0d114324786aad1e3",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-500.a3f8d239.woff2"
+  },
+  {
+    "revision": "e8b54199fbd144a34efd02c31dfd0e66",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-500.e8b54199.woff"
+  },
+  {
+    "revision": "0392eea8288a8bd2b7621e14583c3bb6",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-600.0392eea8.ttf"
+  },
+  {
+    "revision": "3117c2d16f1e8cd7221d7c425a9b8c8e",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-600.3117c2d1.woff"
+  },
+  {
+    "revision": "3f74b9bf1807487017e6b71b0e69a640",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-600.3f74b9bf.woff2"
+  },
+  {
+    "revision": "5a374ca902f6141f37360e6bbfbe8b4c",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-600.5a374ca9.eot"
+  },
+  {
+    "revision": "a6924a5e072a58911c8588859f6825d5",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-600.a6924a5e.svg"
+  },
+  {
+    "revision": "189bfdc04fd790dd68e1de69a890c279",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-800.189bfdc0.woff"
+  },
+  {
+    "revision": "3f167b47ccbc00d60ce08497cd1a4623",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-800.3f167b47.svg"
+  },
+  {
+    "revision": "5085a1794f394b9d700cb99c5c659c3b",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-800.5085a179.ttf"
+  },
+  {
+    "revision": "5b99e95293e90d07247882a74d64b1d3",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-800.5b99e952.woff2"
+  },
+  {
+    "revision": "9fc91569b3056144781c54138f768ec5",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-800.9fc91569.eot"
+  },
+  {
+    "revision": "301e90c03e0ae073f1118491356de03b",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-regular.301e90c0.ttf"
+  },
+  {
+    "revision": "7bf99c007acd1baa1f21903b6fda4d65",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-regular.7bf99c00.woff"
+  },
+  {
+    "revision": "cb7189552046407925263921eef3d256",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-regular.cb718955.woff2"
+  },
+  {
+    "revision": "f3fef7e587e6c436df1d7985b2a90ada",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-regular.f3fef7e5.svg"
+  },
+  {
+    "revision": "faf441c82ee1a95f0e8f66dd28edaf69",
+    "url": "/go_to_goal/static/media/montserrat-v14-latin-ext_cyrillic-ext_latin_cyrillic-regular.faf441c8.eot"
+  },
+  {
+    "revision": "b05688670781ebfdb28c3bbd862b0682",
+    "url": "/go_to_goal/static/media/openEye.b0568867.svg"
+  },
+  {
+    "revision": "e1e25daf57692a417bb09c2019e76f37",
+    "url": "/go_to_goal/static/media/party.e1e25daf.svg"
+  },
+  {
+    "revision": "4f7571672afc359eefe827849005c172",
+    "url": "/go_to_goal/static/media/up-arrow.4f757167.svg"
+  }
+]);
